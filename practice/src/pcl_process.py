b3f0df349e30a2d5c6f6b02622c6b528c8a70e26
irobo197 <chienvt197@gmail.com>
2022-07-09 11:37:12 +0700

    Add clustering

--- practice/src/pcl_process.py
+++ practice/src/pcl_process.py
@@ -12,45 +12,41 @@ pcd = o3d.io.read_point_cloud("../../data/kinect_robot/TLS_kitchen.ply")
 
 pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=16), fast_normal_computation=True)
 pcd.paint_uniform_color([0.6, 0.6, 0.6])
-o3d.io.write_point_cloud("../../data/kinect_robot/TLS_kitchen_2.pcd", pcd)
 # o3d.visualization.draw_geometries([pcd]) #Works only outside Jupyter/Colab
 
-"""## 3.2 [INITIATION] 3D Shape Detection with RANSAC"""
-colors_idx = []
-for i in range(10):
-    colors = plt.get_cmap("tab20")(i)
-    colors_idx.append(colors)
-# print(colors_idx)
+# """## 3.2 [INITIATION] 3D Shape Detection with RANSAC"""
+# colors_idx = []
+# for i in range(10):
+#     colors = plt.get_cmap("tab20")(i)
+#     colors_idx.append(colors)
+# # print(colors_idx)
 
-plane_model, inliers = pcd.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=1000)
-[a, b, c, d] = plane_model
-print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-inlier_cloud = pcd.select_by_index(inliers)
-outlier_cloud = pcd.select_by_index(inliers, invert=True)
-inlier_cloud.paint_uniform_color(colors_idx[9][:3])
+# plane_model, inliers = pcd.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=1000)
+# [a, b, c, d] = plane_model
+# print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+# inlier_cloud = pcd.select_by_index(inliers)
+# outlier_cloud = pcd.select_by_index(inliers, invert=True)
+# inlier_cloud.paint_uniform_color(colors_idx[9][:3])
 # outlier_cloud.paint_uniform_color(colors_idx[0])
-
-# o3d.io.write_point_cloud("../data/output/inlier/py/in_9.pcd", inlier_cloud)
-# o3d.io.write_point_cloud("../data/output/outlier/py/rest_9.pcd", outlier_cloud)
 
 # o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
 
-# """## 3.3 [INITIATION] Clustering with DBSCAN"""
+"""## 3.3 [INITIATION] Clustering with DBSCAN"""
 
-# labels = np.array(pcd.cluster_dbscan(eps=0.05, min_points=10))
-# max_label = labels.max()
-# print(f"point cloud has {max_label + 1} clusters")
+labels = np.array(pcd.cluster_dbscan(eps=0.05, min_points=10))
+max_label = labels.max()
+print(f"point cloud has {max_label + 1} clusters")
 
-# colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-# colors[labels < 0] = 0
-# pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+colors[labels < 0] = 0
+pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
 
 # o3d.visualization.draw_geometries([pcd])
 
-# """# Step 4: Scaling and automation
+"""# Step 4: Scaling and automation
 
-# ## 4.1 RANSAC loop for multiple planar shapes detection
-# """
+## 4.1 RANSAC loop for multiple planar shapes detection
+"""
 
 # segment_models={}
 # segments={}
@@ -58,48 +54,49 @@ inlier_cloud.paint_uniform_color(colors_idx[9][:3])
 # rest=pcd
 # for i in range(max_plane_idx):
 #     colors = plt.get_cmap("tab20")(i)
-#     segment_models[i], inliers = rest.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=1000)
+#     _, inliers = rest.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=1000)
 #     segments[i]=rest.select_by_index(inliers)
 #     segments[i].paint_uniform_color(list(colors[:3]))
 #     rest = rest.select_by_index(inliers, invert=True)
 
-#     name = "../data/output/rest_" + str(i)+ ".pcd"
-#     o3d.io.write_point_cloud(name, pcd)
+#     # name = "../data/output/rest_" + str(i)+ ".pcd"
+#     # o3d.io.write_point_cloud(name, pcd)
 
 #     print("pass",i,"/",max_plane_idx,"done.")
     # o3d.visualization.draw_geometries([segments[i] for i in range(len(segments))]+[rest])
 
-# """## 4.2 Refined RANSAC with Euclidean clustering"""
+"""## 4.2 Refined RANSAC with DBSCAN clustering"""
 
 # segment_models={}
-# segments={}
-# max_plane_idx=20
-# rest=pcd
-# d_threshold=0.01
-# for i in range(max_plane_idx):
-#     colors = plt.get_cmap("tab20")(i)
-#     segment_models[i], inliers = rest.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=1000)
-#     segments[i]=rest.select_by_index(inliers)
-#     labels = np.array(segments[i].cluster_dbscan(eps=d_threshold*10, min_points=10))
-#     candidates=[len(np.where(labels==j)[0]) for j in np.unique(labels)]
-#     best_candidate=int(np.unique(labels)[np.where(candidates==np.max(candidates))[0]])
-#     print("the best candidate is: ", best_candidate)
-#     rest = rest.select_by_index(inliers, invert=True)+segments[i].select_by_index(list(np.where(labels!=best_candidate)[0]))
-#     segments[i]=segments[i].select_by_index(list(np.where(labels==best_candidate)[0]))
-#     segments[i].paint_uniform_color(list(colors[:3]))
-#     print("pass",i+1,"/",max_plane_idx,"done.")
+segments={}
+max_plane_idx=20
+rest=pcd
+d_threshold=0.01
+for i in range(max_plane_idx):
+    colors = plt.get_cmap("tab20")(i)
+    _, inliers = rest.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=1000)
+    segments[i]=rest.select_by_index(inliers)
+    labels = np.array(segments[i].cluster_dbscan(eps=d_threshold*10, min_points=10))
+    candidates=[len(np.where(labels==j)[0]) for j in np.unique(labels)]
+    print("candidates: ", candidates)
+    best_candidate=int(np.unique(labels)[np.where(candidates==np.max(candidates))[0]])
+    print("the best candidate is: ", best_candidate)
+    rest = rest.select_by_index(inliers, invert=True) + segments[i].select_by_index(list(np.where(labels!=best_candidate)[0]))
+    segments[i]=segments[i].select_by_index(list(np.where(labels==best_candidate)[0]))
+    segments[i].paint_uniform_color(list(colors[:3]))
+    print("pass",i+1,"/",max_plane_idx,"done.")
 
-# """## 4.3 Euclidean clustering of the rest with DBSCAN"""
+"""## 4.3 Euclidean clustering of the rest with DBSCAN"""
 
-# labels = np.array(rest.cluster_dbscan(eps=0.05, min_points=5))
-# max_label = labels.max()
-# print(f"point cloud has {max_label + 1} clusters")
+labels = np.array(rest.cluster_dbscan(eps=0.05, min_points=5))
+max_label = labels.max()
+print(f"point cloud has {max_label + 1} clusters")
 
-# colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
-# colors[labels < 0] = 0
-# rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
+colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
+colors[labels < 0] = 0
+rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
 
-# # o3d.visualization.draw_geometries([segments.values()])
-# # o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)]+[rest])
-# o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)]+[rest],zoom=0.3199,front=[0.30159062875123849, 0.94077325609922868, 0.15488309545553303],lookat=[-3.9559999108314514, -0.055000066757202148, -0.27599999308586121],up=[-0.044411423633999815, -0.138726419067636, 0.98753122516983349])
-# # o3d.visualization.draw_geometries([rest])
+# o3d.visualization.draw_geometries([segments.values()])
+# o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)]+[rest])
+o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)]+[rest],zoom=0.3199,front=[0.30159062875123849, 0.94077325609922868, 0.15488309545553303],lookat=[-3.9559999108314514, -0.055000066757202148, -0.27599999308586121],up=[-0.044411423633999815, -0.138726419067636, 0.98753122516983349])
+# o3d.visualization.draw_geometries([rest])
---
+++ practice/src/ransac_segmen.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ> point_cloud_t;
+typedef pcl::PointCloud<pcl::PointXYZRGB> point_cloud_rgb_t;
+
+int color_picker[10][3] = {
+    {0  , 153, 204},
+    {0  , 255, 255},
+    {51 , 204, 51 },
+    {204, 204, 0  },
+    {255, 102, 0  },
+    {255, 102, 204},
+    {51 , 102, 255},
+    {153, 51 , 153},
+    {153, 51 , 51 },
+    {255, 204, 0  }
+};
+
+// void XYZtoXYZRGB(boost::shared_ptr<point_cloud_t> &input,
+//                 boost::shared_ptr<point_cloud_rgb_t> &output)
+void XYZtoXYZRGB(point_cloud_t &input,
+                point_cloud_rgb_t &output)
+{
+    output.points.resize(input.points.size());
+    output.width = input.width;
+    output.height = input.height;
+    // std::cout<<cloud_rgb->header<<" "<<cloud_rgb->is_dense<<std::endl;
+    // std::cout<<cloud->header<<" "<<cloud->is_dense<<std::endl;
+
+    for (size_t i = 0; i < input.points.size(); i++) 
+    {
+        output.points[i].x = input.points[i].x;
+        output.points[i].y = input.points[i].y;
+        output.points[i].z = input.points[i].z;
+        
+        output.points[i].r = 255;
+        output.points[i].g = 0;
+        output.points[i].b = 0;
+    }
+}
+
+void extract_indices(point_cloud_rgb_t::Ptr &cloud_rgb,
+                point_cloud_rgb_t::Ptr &inlierPoints_neg,
+                pcl::PointIndices::Ptr &inliers)
+{
+    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+    extract.setInputCloud (cloud_rgb);
+    extract.setIndices (inliers);
+    extract.setNegative (true);//false
+    extract.filter (*inlierPoints_neg);
+}
+
+void segmen_plane(point_cloud_rgb_t::Ptr &cloud_rgb,
+                point_cloud_rgb_t::Ptr &segment,
+                int max_plane_idx)
+{
+    point_cloud_rgb_t::Ptr inlierPoints (new point_cloud_rgb_t ()),
+                        inlierPoints_neg (new point_cloud_rgb_t ());
+
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+    point_cloud_rgb_t *segments;
+    segments = new point_cloud_rgb_t[max_plane_idx];
+
+    for (int i = 0; i < max_plane_idx; i++)
+    {
+        uint8_t r = color_picker[i][0];
+        uint8_t g = color_picker[i][1];
+        uint8_t b = color_picker[i][2];
+        
+        // Create the segmentation object.
+        pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+        seg.setOptimizeCoefficients (true);       // Enable model coefficient refinement (optional).
+        seg.setInputCloud (cloud_rgb);
+        seg.setModelType (pcl::SACMODEL_PLANE);   // Configure the object to look for a plane.
+        seg.setMethodType (pcl::SAC_RANSAC);      // Use RANSAC method.
+        seg.setMaxIterations (1000);
+        seg.setDistanceThreshold (0.01);          // Set the maximum allowed distance to the model.
+        //seg.setRadiusLimits(0, 0.1);     // cylinder, Set minimum and maximum radius of the cylinder.
+        seg.segment (*inliers, *coefficients);
+
+        pcl::copyPointCloud<pcl::PointXYZRGB>(*cloud_rgb, *inliers, *inlierPoints);
+        // std::cout << inliers->indices.size() << std::endl;
+        // std::cout << inlierPoints->points.size() << std::endl;
+        // for (int m=0; m<inliers->indices.size(); m++)
+        for (int m=0; m<inlierPoints->points.size(); m++)
+        {   
+            uint32_t rgb = (r << 16) | (g << 8) | b;
+            inlierPoints->points[m].rgba = *(uint32_t *)(&rgb); // makes the point red
+        }
+        pcl::copyPointCloud<pcl::PointXYZRGB>(*inlierPoints, segments[i]);
+        *segment += segments[i];
+        extract_indices(cloud_rgb, inlierPoints_neg, inliers);
+        pcl::copyPointCloud<pcl::PointXYZRGB>(*inlierPoints_neg, *cloud_rgb);
+    }
+}
+
+
+int main (int argc, char** argv)
+{
+    point_cloud_rgb_t::Ptr cloud_rgb (new point_cloud_rgb_t),
+                        segment (new point_cloud_rgb_t);
+
+    pcl::io::loadPCDFile<pcl::PointXYZRGB> ("../../data/kinect_robot/world_filtered.pcd", *cloud_rgb);
+    // XYZtoXYZRGB(*cloud, *cloud_rgb);
+
+    segmen_plane(cloud_rgb, segment, 1);
+
+    segmen_plane(cloud_rgb, segment, 10);
+
+    pcl::io::savePCDFile<pcl::PointXYZRGB>("../data/output/world_filtered_segmen_cpp.pcd", *segment);
+
+    return (0);
+}
